69aca3a57240bd4a4671c449b9690064fb54e593
Thamireslima9 <thamires.silva@aluno.ifsp.edu.br>
2024-08-28 10:23:41 -0300

    28/08 aula 3

--- Compra_tela.Designer.cs
+++ Compra_tela.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(478, 69);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 0;
             // 
             // Compra_tela
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1362, 639);
+            this.Controls.Add(this.checkedListBox1);
             this.Name = "Compra_tela";
             this.Text = "Compra_tela";
             this.Load += new System.EventHandler(this.Compra_tela_Load);
@@ -43,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
